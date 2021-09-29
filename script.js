7274b835e0995f564782d3bab1b5483e3cf8e9cb
iikim <iikim2511@gmail.com>
2021-09-28 23:07:40 -0400

    dfgs

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ window.onload = () => {
   }, 4000);
 };
 
+/* 🍀c43 메뉴버튼 누르면 메뉴가 밖에서 안으로 transform-transition함
+ 1. transform 으로 효과 넣음
+ 2. .change.container의 child인 .sidebar에 효과 넣음
+*/
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
